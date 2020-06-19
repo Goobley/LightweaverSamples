52e93041bb0f22dcfc6ab0c3abb73db92659f16d
cmo <c.osborne.1@research.gla.ac.uk>
2020-06-19 21:44:13 +0100

    Added SimpleCtxTest that iterates a context in CRD

---
+++ SimpleCtxTest.py
@@ -0,0 +1,39 @@
+from lightweaver.fal import Falc82
+from lightweaver.rh_atoms import H_6_atom, H_6_CRD_atom, H_3_atom, C_atom, O_atom, OI_ord_atom, Si_atom, Al_atom, CaII_atom, Fe_atom, FeI_atom, He_9_atom, He_atom, He_large_atom, MgII_atom, N_atom, Na_atom, S_atom
+import lightweaver as lw
+import matplotlib.pyplot as plt
+import time
+import pickle
+import numpy as np
+from tqdm import tqdm
+plt.ion()
+
+def iterate_ctx_crd(ctx, Nscatter=10, NmaxIter=500):
+    for i in range(NmaxIter):
+        dJ = ctx.formal_sol_gamma_matrices()
+        if i < Nscatter:
+            continue
+        delta = ctx.stat_equil()
+
+        if dJ < 3e-3 and delta < 1e-3:
+            print(i)
+            print('----------')
+            return
+
+def synth_spectrum(atmos, depthData=False):
+    atmos.convert_scales()
+    atmos.quadrature(5)
+    aSet = lw.RadiativeSet([H_6_atom(), C_atom(), OI_ord_atom(), Si_atom(), Al_atom(), CaII_atom(), Fe_atom(), He_9_atom(), MgII_atom(), N_atom(), Na_atom(), S_atom()])
+    aSet.set_active('H', 'He', 'Ca', 'O')
+    spect = aSet.compute_wavelength_grid()
+
+    eqPops = aSet.compute_eq_pops(atmos)
+
+    ctx = lw.Context(atmos, spect, eqPops, Nthreads=8)
+    if depthData:
+        ctx.depthData.fill = True
+    iterate_ctx_crd(ctx)
+    return ctx
+
+atmos = Falc82()
+ctx = synth_spectrum(atmos)
